b17eb3ddb5d9bcda7b99e135bcc54aa26a0cbbae
SainTod <alexx.dudkin@gmail.com>
2016-07-09 20:31:43 +0300

    Target Library

---
+++ GraphicsTest/GraphicLib/GraphicLib.h
@@ -0,0 +1,13 @@
+//
+//  GraphicLib.h
+//  GraphicLib
+//
+//  Created by Vlad on 09/07/2016.
+//  Copyright © 2016 Vlad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GraphicLib : NSObject
+
+@end
---
+++ GraphicsTest/GraphicLib/GraphicLib.m
@@ -0,0 +1,13 @@
+//
+//  GraphicLib.m
+//  GraphicLib
+//
+//  Created by Vlad on 09/07/2016.
+//  Copyright © 2016 Vlad. All rights reserved.
+//
+
+#import "GraphicLib.h"
+
+@implementation GraphicLib
+
+@end
